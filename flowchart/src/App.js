e10476461872f3a159700ebccf7e1459807c3817
meyges <mariya.sf@gmail.com>
2020-08-10 18:05:28 -0700

    adding flowchart test

---
+++ flowchart/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import BasicFlow from './FlowChart';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <BasicFlow />
+    </div>
+  );
+}
+
+export default App;
